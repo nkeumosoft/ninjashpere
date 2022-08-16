a7c5a790b85cf1736e6dd802b410a065a9e35b53
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-08-16 15:46:06 +0100

    Some updates about project configuration

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,4 +1,4 @@
-"""website URL Configuration
+"""website URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('websitecore.urls', 'core'), namespace="core")),
-]
+    path("admin/", admin.site.urls),
+    path("", include(("websitecore.urls", "websitecore"), namespace="core")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
