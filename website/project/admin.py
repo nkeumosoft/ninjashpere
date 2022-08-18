b4a08ade625ed58f8d691127f548b8f960eea44d
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-08-18 11:08:45 +0100

    Construction of models

--- website/project/admin.py
+++ website/project/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Technology
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "project_name", "project_link")
+    list_filter = ("project_technologies",)
+
+
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ("id", "tech_name")
+
+
+admin.site.register(Technology, TechnologyAdmin)
+admin.site.register(Project, ProjectAdmin)
--- website/project/models.py
+++ website/project/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Technology(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    tech_name = models.CharField(max_length=50, blank=False, null=False)
+    tech_logo = models.FileField(null=False, upload_to="logos/technologies/")
+
+    def __str__(self):
+        return self.tech_name
+
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    project_name = models.CharField(max_length=50, blank=False, null=False)
+    project_description = models.TextField()
+    project_image = models.ImageField(null=True, upload_to="images/projects/")
+    project_technologies = models.ManyToManyField(Technology)
+    project_link = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.project_name
