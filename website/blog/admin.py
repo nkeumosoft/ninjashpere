3afe179c4445d27684c248adad2d9961033e284d
noutcheu libert <libert.noutcheu@gmail.com>
2022-08-18 13:54:27 +0100

    create post comment models and add to admin file

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,38 @@
+from blog.forms.member_forms import (CustomUserChangeForm,
+                                     CustomUserCreationForm)
+from blog.models import Member
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Member
+    list_display = (
+        "email",
+        "is_staff",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff"),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(Member, CustomUserAdmin)
--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,39 @@
+from blog.basemanager import MemberManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Member(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(_("email adresse"), unique=True)
+    password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["password"]
+    object = MemberManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    picture = models.ImageField(upload_to="media/title_post")
+    author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
+    publish_date = models.DateTimeField(auto_now=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    likes = models.PositiveIntegerField()
+    publish = models.BooleanField(default=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Member, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_comment = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=255)
+    likes = models.PositiveIntegerField()
