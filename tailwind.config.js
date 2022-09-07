0e97af6d87fb7b1a828dacd222b95e0225d234a9
tchassijordan <tchassijordan@gmail.com>
2022-09-07 12:17:58 +0100

    chore: install tailwind forms and add website font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const taiwindForms = require("@tailwindcss/forms");
+
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+        display: ["Lexend", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
-  plugins: [],
+  plugins: [taiwindForms],
 };
