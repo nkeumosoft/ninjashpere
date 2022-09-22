1d558bacd5f946918e16e369ecf3a0552b908f97
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-09-22 13:38:50 +0100

    Adding code skeleton of another views

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from blog.models import Post
+from blog.models import Post, Comment, Member
+
+
+def like_post_view(request, pk):
+    ...
+
+
+def like_comment_view(request, pk):
+    ...
 
 
 class PostListView(ListView):
@@ -21,4 +30,50 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
     def get_context_data(self, **kwargs):
+        ...
+
+
+class MemberView(View):
+    template_name = ""
+
+    def post(self, request, *args, **kwargs):
+        ...
+
+    def get(self, request, *args, **kwargs):
+        ...
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = ""
+    fields = ["post_type", "title", "content", "key_words", "picture"]
+
+    def post(self, request, *args, **kwargs):
+        ...
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = ""
+    fields = ["post_type", "title", "content", "key_words", "picture"]
+
+    def post(self, request, *args, **kwargs):
+        ...
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = ""
+    fields = ["content"]
+
+    def post(self, request, *args, **kwargs):
+        ...
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    template_name = ""
+    fields = ["content"]
+
+    def post(self, request, *args, **kwargs):
         ...
